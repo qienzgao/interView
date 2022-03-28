cf62a127c0e7900e7dca1c58f0e238b9a2de2bfd
Qien Gao <qiengao@qiens-mbp.myfiosgateway.com>
2022-03-28 12:52:41 -0400

    add about links and favicon, change og:image to gif

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -49,44 +49,60 @@ class MainPage extends React.Component {
                                 <img src="Alex.jpg">
                                 </img>
                                 <div className="personal-links" >
-                                    <span>Alex Clark</span>
+                                    <h3>Alex Clark</h3>
                                     <br/>
                                     <a href='https://www.linkedin.com/in/alex-b-clark-wrightstate/' target="_blank">LinkedIn</a>
                                     <br/>
                                     <a href='https://github.com/Alyx-Clark' target="_blank">Github</a>
+                                    <br/>
+                                    <a href='https://angel.co/u/alex-clark-36' target="_blank">AngelList</a>
+                                    <br/>
+                                    <a href='' target="_blank">Portfolio</a>
                                 </div>
                             </button>
                             <button className='sliding-item' >
                                 <img src="Ian.jpg">
                                 </img>
                                 <div className="personal-links">
-                                    <span>Ian Farr</span>
+                                    <h3>Ian Farr</h3>
                                     <br/>
                                     <a href='https://www.linkedin.com/in/ian-farr/' target="_blank">LinkedIn</a>
                                     <br />
                                     <a href='https://github.com/IanFarr' target="_blank">Github</a>
+                                    <br />
+                                    <a href='https://angel.co/u/ian-farr-1' target="_blank">AngelList</a>
+                                    <br />
+                                    <a href='https://ianfarr.dev' target="_blank">Portfolio</a>
                                 </div>
                             </button>
                             <button className='sliding-item' >
                                 <img src="Jonathan.jpg">
                                 </img>
                                 <div className="personal-links">
-                                    <span>Jonathan Kim</span>
+                                    <h3>Jonathan Kim</h3>
                                     <br />
                                     <a href='https://github.com/hwkcode' target="_blank">LinkedIn</a>
                                     <br />
                                     <a href='https://github.com/hwkcode' target="_blank">Github</a>
+                                    <br />
+                                    <a href='https://angel.co/u/jonathan-kim-68' target="_blank">AngelList</a>
+                                    <br />
+                                    <a href='' target="_blank">Portfolio</a>
                                 </div>
                             </button>
                             <button className='sliding-item'>
                                 <img src="Qien.jpg">
                                 </img>
                                 <div className="personal-links">
-                                    <span>Qien Gao</span>
+                                    <h3>Qien Gao</h3>
                                     <br />
                                     <a href="https://github.com/qienzgao" target="_blank">LinkedIn</a>
                                     <br />
                                     <a href="https://www.linkedin.com/in/qien-gao-1390071a7/" target="_blank">Github</a>
+                                    <br />
+                                    <a href='https://angel.co/u/qien-gao' target="_blank">AngelList</a>
+                                    <br />
+                                    <a href='https://qiengao.com/' target="_blank">Portfolio</a>
                                 </div>
                             </button>
                         </div>
